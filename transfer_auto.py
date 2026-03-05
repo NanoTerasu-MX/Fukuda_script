fa86673ebd7a56ba252e935d921df83f2e140c62
Fukuda-akiya <hukuda.akiya.s3@dc.tohoku.ac.jp>
2026-03-05 16:14:03 +0900

    edit transfer_auto.py

--- transfer_auto.py
+++ transfer_auto.py
@@ -402,7 +402,7 @@ class AutoTransferAndProcess:
         p = Path(dataset_path)
         base_parent = p.parents[2]
         dest_subdir = base_parent.relative_to("/data")
-        write_kamo_proc_path = os.path.join(self.destination_path_via_aoba, dest_subdir, "dataset_paths_for_kamo.txt")
+        write_kamo_proc_path = os.path.join(self.destination_path_via_aoba, dest_subdir)
         tmp_path = Path(dataset_path)
         output_path = tmp_path.relative_to("/data")
         output_path = os.path.join(self.destination_path_via_aoba, output_path)
